3138cd806728e507396a0c7a9d5260fd2f29870e
Pierre Bourgeois <dragonralph@gmail.com>
2016-05-08 17:48:42 +0100

    Firebox resolved

--- app/webroot/js/app.js
+++ app/webroot/js/app.js
@@ -49,7 +49,6 @@ Terminal = {
         Terminal.addConsole(t("global.welcome"));
         Terminal.updateType();
         $("html").on("keypress", Terminal.callbackKey);
-        $("html").keypress(Terminal.callbackKey);
         $("html").bind("keydown keypress", Terminal.backKey);
     },
     /**
